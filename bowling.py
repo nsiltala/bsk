bdd9af7baa07fb2f8558dab4429d7d220424b350
Niko Siltala <nisiltal@student.oulu.fi>
2024-10-28 18:32:42 +0200

    US8 - Test 1

--- bowling.py
+++ bowling.py
@@ -5,6 +5,7 @@ from frame import Frame
 class BowlingGame:
 
     def __init__(self):
+        self._bonus_throw = None
         self._frames = []
     
     def add_frame(self, frame: Frame) -> None:
@@ -21,7 +22,10 @@ class BowlingGame:
         total_score = 0
         for i, frame in enumerate(self._frames):
             if frame.is_spare():
-                frame.set_bonus(self._frames[i + 1].get_first_throw())
+                if i == len(self._frames) - 1:
+                    frame.set_bonus(self._bonus_throw)
+                else:
+                    frame.set_bonus(self._frames[i + 1].get_first_throw())
             if frame.is_strike():
                 if self._frames[i + 1].is_strike():
                     frame.set_bonus(self._frames[i + 1].get_first_throw() + self._frames[i + 1].get_second_throw() +
@@ -32,7 +36,7 @@ class BowlingGame:
         return total_score
 
     def set_first_bonus_throw(self, bonus_throw: int) -> None:
-        pass
+        self._bonus_throw = bonus_throw
 
     def set_second_bonus_throw(self, bonus_throw: int) -> None:
         pass
